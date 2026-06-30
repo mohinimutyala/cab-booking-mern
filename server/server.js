a4b26102f1a8513228e629a18bb612d55c7abac2
Mutyala Mohini Sri Mani <mohini.mutyala@sasi.ac.in>
2026-06-30 21:16:09 +0530

    Setup backend structure and Express server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("UCab Backend Running Successfully");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
